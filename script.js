a60f99ff7dacba42d6ff983119d7fd4db7283fad
SEGACD32XMODEL1 <33665206+SEGACD32XMODEL1@users.noreply.github.com>
2025-03-09 16:09:38 -0500

    Some changes + more designs
    Added loading screen, jpgs for grid, more designs. Removed border, phone number, load times.

---
+++ script.js
@@ -0,0 +1,83 @@
+$(window).on("load", function () {
+    // PAGE IS FULLY LOADED
+    // FADE OUT YOUR OVERLAYING DIV
+    $("#overlay").fadeOut();
+});
+
+const imgs = [
+    "4-20-2025_Easter_Sunrise_Service",
+    "AshWednesday2025",
+    "ballice",
+    "basklove",
+    "boards",
+    "coffee",
+    "doorpolicy",
+    "EasterSunday2025",
+    "empwomen",
+    "eployparty",
+    "fallback",
+    "fallcross",
+    "GoodFriday2025",
+    "gradsun",
+    "greens",
+    "HolySaturday2025",
+    "HolyWeek2025",
+    "kitchenshow",
+    "kolaches",
+    "lifeneon",
+    "MaundyThursday2025",
+    "ophouse",
+    "PalmSunday2025",
+    "pecan",
+    "Plarnathon_March_2025",
+    "SpringFeverSunday2025",
+    "wayseek",
+    "wedmeal",
+];
+
+shuffle(imgs);
+
+const imgFolder = "./img/";
+const thumsFolder = "./thumbs/";
+const dotPng = ".png";
+const dotJpg = ".jpg";
+
+for (let i = 0; i < imgs.length; i++) {
+    let aTag = document.createElement("a");
+    aTag.href = imgFolder + imgs[i] + dotPng;
+    aTag.dataset.fslightbox = "gallery";
+
+    let imgTag = document.createElement("img");
+    imgTag.src = thumsFolder + imgs[i] + dotJpg;
+    imgTag.className = "thum";
+    imgTag.setAttribute("rel", "preload");
+    imgTag.setAttribute("as", "image");
+
+    aTag.appendChild(imgTag);
+    document.getElementById("divGrid").appendChild(aTag);
+}
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
+    }
+}
+
+/*
+<a href="./img/SpringFeverSunday2025.png"
+data-fslightbox="gallery">
+    <img src="./img/SpringFeverSunday2025.png"
+    class="thum" />
+</a>
+*/
